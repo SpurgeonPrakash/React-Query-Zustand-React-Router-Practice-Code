6cc0a615bce8ea7282f2cfd32166f8a3ecc121f9
SpurgeonTara <spurgeon.gm@gmail.com>
2023-08-29 13:38:33 +0530

    change the useQuery configs to default options

--- src/main.tsx
+++ src/main.tsx
@@ -7,24 +7,21 @@ import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import App from "./App";
 import "./index.css";
 
-const queryClient = new QueryClient({
-  defaultOptions: {
-    queries: {
-      // Retry 3 times for unsuccessfull requests(default 3)
-      retry: 3,
-      // The data will be cached for 5 minutes(default 5m)
-      cacheTime: 300_000, //5m
-      // staleTime is when the reactQuery considers the cached data as old data(not fresh data). Here we are setting it to 10s, default is 0s
-      staleTime: 10 * 1_000, //10s
-      // staledata will be fetched again for fresh data on window focus(default: true)
-      refetchOnWindowFocus: false,
-      // staledata will be fetched again for fresh data on network reconnection(default: true)
-      refetchOnReconnect: false,
-      // staledata will be fetched again for fresh data when component mounts(default: true)
-      refetchOnMount: false,
-    },
-  },
-});
+const queryClient = new QueryClient();
+// Default options work fine. One thing to change is staleTime, we will set this option per request when we use useQueryHook
+// queryClient config object. For all QUeries
+//   {
+//   defaultOptions: {
+//     queries: {
+//       retry: 3,
+//       cacheTime: 300_000, //5m
+//       staleTime: 10 * 1_000, //10s
+//       refetchOnWindowFocus: false,
+//       refetchOnReconnect: false,
+//       refetchOnMount: false,
+//     },
+//   },
+// }
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
