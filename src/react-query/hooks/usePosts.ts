0966ba32b0277db03d87ebaa1ed67f0fd6ac13b7
SpurgeonTara <spurgeon.gm@gmail.com>
2023-08-29 16:27:52 +0530

    Add Query WIse Stale Time and Implement parameterized queries in useQuery

---
+++ src/react-query/hooks/usePosts.ts
@@ -0,0 +1,26 @@
+import { useQuery } from "@tanstack/react-query";
+import axios from "axios";
+
+export interface Post {
+  id: number;
+  title: string;
+  body: string;
+  userId: number;
+}
+
+// Setting Stale time Per Request
+const usePosts = (userId: number | undefined) =>
+  useQuery<Post[], Error>({
+    queryKey: userId ? ["users", userId, "posts"] : ["posts"],
+    queryFn: () =>
+      axios
+        .get("https://jsonplaceholder.typicode.com/posts", {
+          params: {
+            userId,
+          },
+        })
+        .then((res) => res.data),
+    staleTime: 1 * 60 * 1000, //1m
+  });
+
+export default usePosts;
