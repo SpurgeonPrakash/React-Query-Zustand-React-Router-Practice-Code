1acf1bc461eaadd0117733b8904fba33c145f3f4
SpurgeonTara <spurgeon.gm@gmail.com>
2023-08-29 19:33:12 +0530

    Saving Todo in cache on Mutate to show quickly instead waiting for response to resolve and reverting this action back on error

--- src/react-query/TodoForm.tsx
+++ src/react-query/TodoForm.tsx
@@ -3,28 +3,54 @@ import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { Todo } from "./hooks/useTodos";
 import axios from "axios";
 
+interface AddTodoContext {
+  previousTodos: Todo[];
+}
+
 const TodoForm = () => {
   const queryClient = useQueryClient();
-  const addTodo = useMutation<Todo, Error, Todo>({
+  const addTodo = useMutation<Todo, Error, Todo, AddTodoContext>({
     mutationFn: (todo: Todo) => {
       return axios
         .post<Todo>("https://jsonplaceholder.typicode.com/todos", todo)
         .then((res) => res.data);
     },
+    onMutate: (newTodo: Todo) => {
+      const previousTodos = queryClient.getQueryData<Todo[]>(["todos"]) || [];
+
+      queryClient.setQueryData<Todo[]>(["todos"], (todos) => [
+        newTodo,
+        ...(todos || []),
+      ]);
+
+      if (ref.current) ref.current.value = "";
+
+      return { previousTodos };
+    },
     onSuccess: (savedTodo, newTodo) => {
+      // savedTodo is the todo We get from the response
+      // new todo is the variable, the post body of request(todo object)
       // console.log(savedTodo);
       // APPROACH 1: Invalidate the cache
       // queryClient.invalidateQueries({
       //   queryKey: ["todos"],
       // });
-
       // APPROACH 2: Updating the data in cache directly
-      queryClient.setQueryData<Todo[]>(["todos"], (todos) => [
-        savedTodo,
-        ...(todos || []),
-      ]);
+      // queryClient.setQueryData<Todo[]>(["todos"], (todos) => [
+      //   savedTodo,
+      //   ...(todos || []),
+      // ]);
+      // if (ref.current) ref.current.value = "";
 
-      if (ref.current) ref.current.value = "";
+      queryClient.setQueryData<Todo[]>(["todos"], (todos) =>
+        todos?.map((todo) => (todo === newTodo ? savedTodo : todo))
+      );
+    },
+    onError: (error, newTodo, context) => {
+      if (!context) {
+        return;
+      }
+      queryClient.setQueryData<Todo[]>(["todos"], context.previousTodos);
     },
   });
   const ref = useRef<HTMLInputElement>(null);
