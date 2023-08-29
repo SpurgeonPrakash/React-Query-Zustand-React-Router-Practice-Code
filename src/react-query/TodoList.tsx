b96674423d41e1ee3c087d742a1cc004420bfdb7
SpurgeonTara <spurgeon.gm@gmail.com>
2023-08-29 13:15:28 +0530

    Seperate fetching logic using useTodos custom hook

--- src/react-query/TodoList.tsx
+++ src/react-query/TodoList.tsx
@@ -1,24 +1,11 @@
-import axios from "axios";
-import React, { useEffect, useState } from "react";
-import { useQuery } from "@tanstack/react-query";
-
-interface Todo {
-  id: number;
-  title: string;
-  userId: number;
-  completed: boolean;
-}
+import useTodos from "./hooks/useTodos";
 
 const TodoList = () => {
-  const fetchTodos = () =>
-    axios
-      .get<Todo[]>("https://jsonplaceholder.typicode.com/todos")
-      .then((res) => res.data);
+  const { data: todos, error, isLoading } = useTodos();
 
-  const { data: todos, error } = useQuery<Todo[], Error>({
-    queryKey: ["todos"],
-    queryFn: fetchTodos,
-  });
+  if (isLoading) {
+    return <h1>Loading...</h1>;
+  }
 
   if (error) return <p>{error.message}</p>;
 
