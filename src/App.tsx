f6961618592ae9b4562b284ed79662372efaf71f
SpurgeonTara <spurgeon.gm@gmail.com>
2023-08-31 11:55:05 +0530

    Using basic Reducer hook from react

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,11 @@ import "./App.css";
 import PostList from "./react-query/PostList";
 import TodoForm from "./react-query/TodoForm";
 import TodoList from "./react-query/TodoList";
+import Counter from "./state-management/Counter";
 // import TodoList from "./react-query/TodoList";
 
 function App() {
-  return (
-    <>
-      <TodoForm />
-      <TodoList />
-    </>
-  );
+  return <Counter />;
 }
 
 export default App;
