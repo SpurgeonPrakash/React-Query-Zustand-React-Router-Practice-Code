fcded0259c1475df0810a5b1e2c6d5492096c06f
SpurgeonTara <spurgeon.gm@gmail.com>
2023-08-31 16:57:56 +0530

    use Zustand to manege global state of counter

--- src/state-management/NavBar.tsx
+++ src/state-management/NavBar.tsx
@@ -1,12 +1,18 @@
 import { useContext } from "react";
 import LoginStatus from "./auth/LoginStatus";
 import TasksContext from "./tasks/tasksContext";
+import useCounterStore from "./counter/store";
 
 const NavBar = () => {
   const { tasks } = useContext(TasksContext);
+  const { counter } = useCounterStore();
   return (
     <nav className="navbar d-flex justify-content-between">
-      <span className="badge text-bg-secondary">{tasks.length}</span>
+      <div>
+        counter: <span className="badge text-bg-secondary me-1">{counter}</span>
+        tasks:{" "}
+        <span className="badge text-bg-secondary me-1">{tasks.length}</span>
+      </div>
       <LoginStatus />
     </nav>
   );
