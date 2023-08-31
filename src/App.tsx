7ff8daf2b2151e65e618faa2215efbe0e716202c
SpurgeonTara <spurgeon.gm@gmail.com>
2023-08-31 12:33:53 +0530

    More useReducer Practice

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,12 @@ import PostList from "./react-query/PostList";
 import TodoForm from "./react-query/TodoForm";
 import TodoList from "./react-query/TodoList";
 import Counter from "./state-management/Counter";
+import LoginStatus from "./state-management/LoginStatus";
 import TaskList from "./state-management/TaskList";
 // import TodoList from "./react-query/TodoList";
 
 function App() {
-  return <TaskList />;
+  return <LoginStatus />;
 }
 
 export default App;
