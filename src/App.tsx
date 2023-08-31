cfbfaae0f358b121104a1fcf1064d63514d6c3fa
SpurgeonTara <spurgeon.gm@gmail.com>
2023-08-31 12:19:52 +0530

    More Complex useReducer hook

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,11 @@ import PostList from "./react-query/PostList";
 import TodoForm from "./react-query/TodoForm";
 import TodoList from "./react-query/TodoList";
 import Counter from "./state-management/Counter";
+import TaskList from "./state-management/TaskList";
 // import TodoList from "./react-query/TodoList";
 
 function App() {
-  return <Counter />;
+  return <TaskList />;
 }
 
 export default App;
